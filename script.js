a0fec5050917b8496a68281cd1c59f013ac54477
anthonyescobar <aescob13@lion.lmu.edu>
2016-10-24 20:56:56 -0700

    Added Basic Leadership and Skills Outline

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+$('#accordion').on('hidden.bs.collapse', function () {
+//do something...
+})
 
+$('#accordion .accordion-toggle').click(function (e){
+  var chevState = $(e.target).siblings("i.indicator").toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+  $("i.indicator").not(chevState).removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+});
